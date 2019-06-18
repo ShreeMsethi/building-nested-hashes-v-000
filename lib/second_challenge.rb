dea5ad5c50ec930a7faf2f7e259382327fa5612b
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-06-18 04:04:37 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,6 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {patriarch: {}, matriarch: {}, hero: {}, hero_friends: []}, 
-    :capulet => {patriarch: {}, matriarch: {}, heroine: {}, heroine_friends: [] }
+    :montague => {patriarch: {name:, age: }, matriarch: {name:, age:}, hero: {name: , age: , status: }, hero_friends: []}, 
+    :capulet => {patriarch: {name: ,age: }, matriarch: {name:, age: ,}, heroine: {name: , age: , status: }, heroine_friends: [] }
                   }
 end
