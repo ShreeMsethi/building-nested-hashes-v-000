9c4ba1fcae09f74d5cccc49b398352338b8faa5b
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-06-18 04:08:29 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,11 +2,8 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {
-        name: "", age:
-        },
-      :hero => {name: , age: , status:
-        },
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo" , age: "15" , status: "alive"},
       :hero_friends => []
    }, 
    :capulet => {
